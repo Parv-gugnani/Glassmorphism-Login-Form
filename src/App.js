07645342b4703fb490a5f6fd4749f763321bf740
Parv-gugnani <9765.parv@gmail.com>
2023-08-26 14:02:08 +0530

    completed!

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import logo from "./logo.svg";
 import "./App.css";
 import LoginForm from "./LoginForm";
-import HomePage from "./HomePage"; // Import the HomePage component
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/login" component={LoginForm} />
-          <Route path="/" exact component={HomePage} />{" "}
-          {/* Define the route for the home page */}
-          {/* Add more routes for other pages if needed */}
-        </Switch>
-      </Router>
+      <LoginForm />
     </div>
   );
 }
